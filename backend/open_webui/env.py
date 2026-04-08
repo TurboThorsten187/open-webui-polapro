3bf1eceeeda9c10a93c7a1dad44d45a2f4f1bd3e
Micha Walter <waltermi96328@th-nuernberg.de>
2026-04-08 13:58:12 +0200

    Stand 0 ohne Änderungen am Code

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -125,9 +125,9 @@ if 'cuda_error' in locals():
 
 SRC_LOG_LEVELS = {}  # Legacy variable, do not remove
 
-WEBUI_NAME = os.environ.get('WEBUI_NAME', 'Open WebUI')
-if WEBUI_NAME != 'Open WebUI':
-    WEBUI_NAME += ' (Open WebUI)'
+WEBUI_NAME = os.environ.get('WEBUI_NAME', 'PoLaPro')
+if WEBUI_NAME != 'PoLaPro':
+    WEBUI_NAME += ' (PoLaPro)'
 
 WEBUI_FAVICON_URL = 'https://openwebui.com/favicon.png'
 
@@ -466,9 +466,9 @@ ENABLE_SIGNUP_PASSWORD_CONFIRMATION = os.environ.get('ENABLE_SIGNUP_PASSWORD_CON
 
 # Optional env vars for creating an admin account on startup
 # Useful for headless/automated deployments
-WEBUI_ADMIN_EMAIL = os.environ.get('WEBUI_ADMIN_EMAIL', '')
-WEBUI_ADMIN_PASSWORD = os.environ.get('WEBUI_ADMIN_PASSWORD', '')
-WEBUI_ADMIN_NAME = os.environ.get('WEBUI_ADMIN_NAME', 'Admin')
+WEBUI_ADMIN_EMAIL = os.environ.get('WEBUI_ADMIN_EMAIL', 'admin@polapro.local')
+WEBUI_ADMIN_PASSWORD = os.environ.get('WEBUI_ADMIN_PASSWORD', 'Admin123!')
+WEBUI_ADMIN_NAME = os.environ.get('WEBUI_ADMIN_NAME', 'PoLaPro Admin')
 
 WEBUI_AUTH_TRUSTED_EMAIL_HEADER = os.environ.get('WEBUI_AUTH_TRUSTED_EMAIL_HEADER', None)
 WEBUI_AUTH_TRUSTED_NAME_HEADER = os.environ.get('WEBUI_AUTH_TRUSTED_NAME_HEADER', None)
