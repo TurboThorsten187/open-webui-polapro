41d411a0b44b56e65279a33e5f00859779c9534a
Micha Walter <waltermi96328@th-nuernberg.de>
2026-04-08 14:14:05 +0200

    feat: add configuration for update toast visibility for admins

--- src/lib/polapro_config.ts
+++ src/lib/polapro_config.ts
@@ -1,4 +1,7 @@
 export const polaproConfig = {
     // Wenn 'false' gesetzt, wird der 'Neuigkeiten' (Changelog) Bildschirm nach dem Laden nicht automatisch angezeigt
     showWhatsNewOnFirstLoad: false,
+    
+    // Wenn 'false' gesetzt, wird die Update-Benachrichtigung für Administratoren unten rechts nicht angezeigt
+    showUpdateToast: false,
 };
